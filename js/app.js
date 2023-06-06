41d908fd413b9043d5afe91d2f4d5c52bf1ab7b3
mdfahim <mdfahimislamoritro@gmail.com>
2023-06-06 20:52:06 +0600

    loader added

--- js/app.js
+++ js/app.js
@@ -36,13 +36,28 @@ const displayPhone =phones=>{
         `
        phonesContainer.appendChild(phoneDiv); 
     });
+    // stop laoder
+    toggleSpinner(false)
 
 }
-
+// Handle search button clicked
 document.getElementById('btn-search').addEventListener('click',function () {
+  // start loader
+  toggleSpinner(true)
   const searchField = document.getElementById('search-field')
   const searchText = searchField.value ;
   loadPhone(searchText);
 })
+
+const toggleSpinner = isLoading =>{
+  const loaderSection = document.getElementById('loader')
+  if(isLoading){
+    loaderSection.classList.remove('d-none')
+  }
+  else{
+    loaderSection.classList.add('d-none')
+  }
+}
+
 
 // loadPhone()
